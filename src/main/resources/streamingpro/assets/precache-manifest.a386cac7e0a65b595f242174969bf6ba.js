52f5a5d6d940185bd3300a343fd1eed7a24e0b1a
WilliamZhu <allwefantasy@gmail.com>
2019-03-28 14:47:50 +0800

    update

---
+++ src/main/resources/streamingpro/assets/precache-manifest.a386cac7e0a65b595f242174969bf6ba.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "b49a90ee8c246157637b5ed142005d12",
+    "url": "/static/media/icons-16.b49a90ee.woff"
+  },
+  {
+    "revision": "4a12194ca75f3578dae5",
+    "url": "/static/css/main.b38594e8.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c6e7874eefb0dacfd50dad73a2de5a17",
+    "url": "/static/media/icons-20.c6e7874e.eot"
+  },
+  {
+    "revision": "02ea096556c3c75856fe",
+    "url": "/static/js/2.c71485b7.chunk.js"
+  },
+  {
+    "revision": "9f52e4014adab067c8ef0f48ec1a4f43",
+    "url": "/static/media/icons-16.9f52e401.eot"
+  },
+  {
+    "revision": "4a12194ca75f3578dae5",
+    "url": "/static/js/main.43ac145b.chunk.js"
+  },
+  {
+    "revision": "9baec0ab0bc3096fb9fe865d6ae29d1d",
+    "url": "/static/media/icons-20.9baec0ab.woff"
+  },
+  {
+    "revision": "d75e6370295e046dd0b170ee42c3fa44",
+    "url": "/static/media/icons-16.d75e6370.ttf"
+  },
+  {
+    "revision": "0f7149769d6b4ee77b646ee5b90c005a",
+    "url": "/static/media/icons-20.0f714976.ttf"
+  },
+  {
+    "revision": "02ea096556c3c75856fe",
+    "url": "/static/css/2.c7599ae4.chunk.css"
+  },
+  {
+    "revision": "9f83108740b5f04e750bcd1ddae5f057",
+    "url": "/index.html"
+  }
+];
